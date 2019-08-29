b668804a9e570ec8c2cf33fdb1e0d083f8ea33f8
Andre Sutherland <andre@perlego.com>
2019-08-29 22:17:52 +0100

    Added jQuery plugin to bundled script

--- src/jquery-plugin.js
+++ src/jquery-plugin.js
@@ -1,45 +1,50 @@
-import $ from "jquery";
-import TextHighlighter from "./text-highlighter";
+/* global jQuery TextHighlighter */
 
-const PLUGIN_NAME = "textHighlighter";
+if (typeof jQuery !== "undefined") {
+  (function($) {
+    "use strict";
 
-function wrap(fn, wrapper) {
-  return function() {
-    wrapper.call(this, fn);
-  };
-}
+    const PLUGIN_NAME = "textHighlighter";
 
-/**
- * The jQuery plugin namespace.
- * @external "jQuery.fn"
- * @see {@link http://docs.jquery.com/Plugins/Authoring The jQuery Plugin Guide}
- */
-
-/**
- * Creates TextHighlighter instance and applies it to the given jQuery object.
- * @param {object} options Same as {@link TextHighlighter} options.
- * @returns {jQuery}
- * @example $('#sandbox').textHighlighter({ color: 'red' });
- * @function external:"jQuery.fn".textHighlighter
- */
-$.fn.textHighlighter = function(options) {
-  return this.each(function() {
-    let el = this,
-      hl;
-
-    if (!$.data(el, PLUGIN_NAME)) {
-      hl = new TextHighlighter(el, options);
-
-      hl.destroy = wrap(hl.destroy, function(destroy) {
-        destroy.call(hl);
-        $(el).removeData(PLUGIN_NAME);
-      });
-
-      $.data(el, PLUGIN_NAME, hl);
+    function wrap(fn, wrapper) {
+      return function() {
+        wrapper.call(this, fn);
+      };
     }
-  });
-};
 
-$.fn.getHighlighter = function() {
-  return this.data(PLUGIN_NAME);
-};
+    /**
+     * The jQuery plugin namespace.
+     * @external "jQuery.fn"
+     * @see {@link http://docs.jquery.com/Plugins/Authoring The jQuery Plugin Guide}
+     */
+
+    /**
+     * Creates TextHighlighter instance and applies it to the given jQuery object.
+     * @param {object} options Same as {@link TextHighlighter} options.
+     * @returns {jQuery}
+     * @example $('#sandbox').textHighlighter({ color: 'red' });
+     * @function external:"jQuery.fn".textHighlighter
+     */
+    $.fn.textHighlighter = function(options) {
+      return this.each(function() {
+        let el = this,
+          hl;
+
+        if (!$.data(el, PLUGIN_NAME)) {
+          hl = new TextHighlighter(el, options);
+
+          hl.destroy = wrap(hl.destroy, function(destroy) {
+            destroy.call(hl);
+            $(el).removeData(PLUGIN_NAME);
+          });
+
+          $.data(el, PLUGIN_NAME, hl);
+        }
+      });
+    };
+
+    $.fn.getHighlighter = function() {
+      return this.data(PLUGIN_NAME);
+    };
+  })(jQuery);
+}
